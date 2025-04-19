166388b9e503db6119ad6d7d9cddd1885845c9af
manel-ar <manelarar931@gmail.com>
2025-04-19 12:05:53 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <React.Fragment>
+            <h2>les frameworks JS</h2>
+            <ul>
+                <li>react</li>
+                <li>vuejs</li>
+                <li>angular</li>
+            </ul>
+        </React.Fragment>
+
+
+
+    )
+}
+
+export default Home
+
+
